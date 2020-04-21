7f032153e9ef35fe253ea3f67bbcc09b87e4dad5
Chongster <ftchong235@yahoo.com>
2020-04-21 13:25:36 +0800

    Logging Errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import Raven from "raven-js";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+Raven.config(
+  "https://cd3377baac644822919059ecb84909d1@o380971.ingest.sentry.io/5207588",
+  {
+    release: "1-0-0",
+    environment: "development-test",
+  }
+).install();
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
